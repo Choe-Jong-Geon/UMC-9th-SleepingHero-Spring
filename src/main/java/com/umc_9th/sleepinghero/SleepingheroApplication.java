8ca3bc110aa76545da20b1549843ca9aa1315ccd
최종건 <jong-geon@jong-geonui-MacBookAir.local>
2025-12-30 12:19:21 +0900

    [Lui] Feat: 깃허브 초기 세팅

---
+++ src/main/java/com/umc_9th/sleepinghero/SleepingheroApplication.java
@@ -0,0 +1,13 @@
+package com.umc_9th.sleepinghero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepingheroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleepingheroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/umc_9th/sleepinghero/SleepingheroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc_9th.sleepinghero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleepingheroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
