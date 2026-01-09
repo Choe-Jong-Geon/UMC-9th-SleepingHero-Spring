1abb0836de01c99b9d3992715c7727cffbc07855
6zeropk <nemm3623@naver.com>
2026-01-09 20:23:58 +0900

    [Zero] Feat : SleepRecord 엔티티 추가

---
+++ src/main/java/com/umc_9th/sleepinghero/domain/sleepRecord/entity/SleepRecord.java
@@ -0,0 +1,36 @@
+package com.umc_9th.sleepinghero.domain.sleepRecord.entity;
+
+import com.umc_9th.sleepinghero.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "sleep_records")
+@Builder
+public class SleepRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "slept_time")
+    private LocalDateTime sleptTime;
+
+    @Column(name = "woke_time")
+    private LocalDateTime wokeTime;
+
+    private boolean success;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+}
