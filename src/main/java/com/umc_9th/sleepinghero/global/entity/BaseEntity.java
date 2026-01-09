85fcfae4c9f6d2a39dca7db5073ecb36bdbbb695
6zeropk <nemm3623@naver.com>
2026-01-09 19:49:22 +0900

    [Zero] Feat : JPA 및 BaseEntity 설정

--- src/main/java/com/umc_9th/sleepinghero/global/entity/BaseEntity.java
+++ src/main/java/com/umc_9th/sleepinghero/global/entity/BaseEntity.java
@@ -1,4 +1,25 @@
 package com.umc_9th.sleepinghero.global.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
+
+    @CreatedBy
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedBy
+    private LocalDateTime updatedAt;
+
 }
