7e3ebfb803049b2cd53c4394294042f60bac1609
최종건 <jong-geon@jong-geonui-MacBookAir.local>
2026-01-14 00:47:57 +0900

    [Loui] chore: 멤버 엔티티 id필드 삭제

--- src/main/java/com/umc_9th/sleepinghero/global/entity/BaseEntity.java
+++ src/main/java/com/umc_9th/sleepinghero/global/entity/BaseEntity.java
@@ -2,11 +2,8 @@ package com.umc_9th.sleepinghero.global.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
