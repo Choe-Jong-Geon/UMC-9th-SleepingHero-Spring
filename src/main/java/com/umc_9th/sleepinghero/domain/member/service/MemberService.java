a8e8e7e406f15d3e3a3d18655bbb458640cd5245
Yongseon-Yoo <jade020823@naver.com>
2026-01-27 13:56:31 +0900

    [ian] Refactor: main 브랜치와 머지하기 위해 member 도메인 구조 통일

--- src/main/java/com/umc_9th/sleepinghero/domain/member/service/MemberService.java
+++ src/main/java/com/umc_9th/sleepinghero/domain/member/service/MemberService.java
@@ -3,14 +3,82 @@ package com.umc_9th.sleepinghero.domain.member.service;
 import com.umc_9th.sleepinghero.domain.member.dto.req.MemberRequestDTO;
 import com.umc_9th.sleepinghero.domain.member.dto.res.MemberResponseDTO;
 import com.umc_9th.sleepinghero.domain.member.entity.Member;
+import com.umc_9th.sleepinghero.domain.member.exception.MemberErrorCode;
+import com.umc_9th.sleepinghero.domain.member.repository.MemberRepository;
+import com.umc_9th.sleepinghero.global.apiPayload.exception.GeneralException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface MemberService {
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
 
-    MemberResponseDTO.CheckTutorialDTO checkTutorial(Long memberId);
+    private final MemberRepository memberRepository;
 
-    MemberResponseDTO.CompleteTutorialResultDTO completeTutorial(Long memberId, MemberRequestDTO.CompleteTutorialDTO request);
 
-    MemberResponseDTO.AgreementsDTO  getAgreements(Long memberId);
 
-    MemberResponseDTO.AgreementsDTO updateAgreements(Long memberId, MemberRequestDTO.UpdateAgreementsDTO request);
+    public MemberResponseDTO.CheckTutorialDTO checkTutorial(Long memberId) {
+
+
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new GeneralException(MemberErrorCode.MEMBER_NOT_FOUND));
+
+        return MemberResponseDTO.CheckTutorialDTO.builder()
+                .finished(member.isTutorialClear())
+                .build();
+    }
+
+
+
+    @Transactional
+    public MemberResponseDTO.CompleteTutorialResultDTO completeTutorial(Long memberId, MemberRequestDTO.CompleteTutorialDTO request) {
+
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new GeneralException(MemberErrorCode.MEMBER_NOT_FOUND));
+
+        member.setTutorialClear(request.isFinished());
+
+//        Member savedMember = memberRepository.save(member);
+
+        return MemberResponseDTO.CompleteTutorialResultDTO.builder()
+                .memberId(member.getId())
+                .finished(member.isTutorialClear())
+                .build();
+    }
+
+
+    public MemberResponseDTO.AgreementsDTO getAgreements(Long memberId) {
+
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new GeneralException(MemberErrorCode.MEMBER_NOT_FOUND));
+
+        return MemberResponseDTO.AgreementsDTO.builder()
+                .serviceTerms(true)
+                .privacyTerms(true)
+                .marketingTerms(member.isMarketingAgreed())
+                .build();
+    }
+
+
+
+    @Transactional
+    public MemberResponseDTO.AgreementsDTO updateAgreements(Long memberId, MemberRequestDTO.UpdateAgreementsDTO request) {
+
+
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new GeneralException(MemberErrorCode.MEMBER_NOT_FOUND));
+
+        member.setMarketingAgreed(request.isMarketingAgreed());
+
+        Member savedMember = memberRepository.save(member);
+
+        return MemberResponseDTO.AgreementsDTO.builder()
+                .serviceTerms(true)
+                .privacyTerms(true)
+                .marketingTerms(savedMember.isMarketingAgreed())
+                .build();
+    }
+
 }
