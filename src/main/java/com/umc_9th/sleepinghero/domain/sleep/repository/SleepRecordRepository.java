8e40f20ecc6c98ea13f1c856374948a6b3aa759d
6zeropk <nemm3623@naver.com>
2026-02-06 20:32:32 +0900

    [Zero] feat : 리뷰 저장 및 피드백 저장 로직 구현

--- src/main/java/com/umc_9th/sleepinghero/domain/sleep/repository/SleepRecordRepository.java
+++ src/main/java/com/umc_9th/sleepinghero/domain/sleep/repository/SleepRecordRepository.java
@@ -5,11 +5,13 @@ import com.umc_9th.sleepinghero.domain.sleep.entity.SleepRecord;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface SleepRecordRepository extends JpaRepository<SleepRecord, Long> {
 
     List<SleepRecord> findAllByMemberAndSuccess(Member member, boolean b);
