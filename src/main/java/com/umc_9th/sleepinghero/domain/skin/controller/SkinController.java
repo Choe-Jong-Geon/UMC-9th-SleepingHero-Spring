48ef3a06eb05ee5ecddff16a4701d48a7124bda5
Yongseon-Yoo <jade020823@naver.com>
2026-02-12 16:06:11 +0900

    [ian] Refactor : swagger 코드 Docs 파일로 재구조화

--- src/main/java/com/umc_9th/sleepinghero/domain/skin/controller/SkinController.java
+++ src/main/java/com/umc_9th/sleepinghero/domain/skin/controller/SkinController.java
@@ -12,19 +12,17 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-public class SkinController {
+public class SkinController implements SkinControllerDocs {
 
     private final SkinService skinService;
 
     @GetMapping("/wardrobe/me/skins")
-    @Operation(summary = "보유 스킨 목록 조회 API", description = "내가 가진 스킨들과 현재 착용 여부를 조회합니다.")
     public ApiResponse<SkinResponseDTO.SkinListDTO> getMySkins(
             @Parameter(hidden = true)  @AuthenticationPrincipal Long memberId
     ) {
         return ApiResponse.onSuccess(GeneralSuccessCode.OK, skinService.getMySkins(memberId));
     }
     @PostMapping("/wardrobe/me/skins/{skinId}/equip")
-    @Operation(summary = "스킨 착용 API", description = "보유한 스킨 중 하나를 골라 캐릭터에 입힙니다.")
     public ApiResponse<String> equipSkin(
             @Parameter(hidden = true) @AuthenticationPrincipal Long memberId,
             @PathVariable Long skinId
