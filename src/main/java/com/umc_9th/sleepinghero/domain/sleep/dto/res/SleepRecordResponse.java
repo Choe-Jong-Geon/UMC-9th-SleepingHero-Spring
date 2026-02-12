846c4b6bd3ccc686e785e8d4e7b271ee93cd29e0
6zeropk <nemm3623@naver.com>
2026-02-12 17:44:26 +0900

    [zero] feat : 수면 기록 조회 응답 ai피드백 및 별점 추가

--- src/main/java/com/umc_9th/sleepinghero/domain/sleep/dto/res/SleepRecordResponse.java
+++ src/main/java/com/umc_9th/sleepinghero/domain/sleep/dto/res/SleepRecordResponse.java
@@ -1,11 +1,16 @@
 package com.umc_9th.sleepinghero.domain.sleep.dto.res;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.util.List;
 
 public record SleepRecordResponse (
         Long recordId,
-        LocalDateTime sleptTime,
-        LocalDateTime wokeTime,
-        boolean isSuccess
+        int star,
+        LocalDate date,
+        long totalMinutes,
+        String summary,
+        List<String> positives,
+        List<String> improvements,
+        String cheering
 ){
 }
--- src/main/java/com/umc_9th/sleepinghero/domain/sleep/repository/SleepFeedBackRepository.java
+++ src/main/java/com/umc_9th/sleepinghero/domain/sleep/repository/SleepFeedBackRepository.java
@@ -5,10 +5,10 @@ import com.umc_9th.sleepinghero.domain.sleep.entity.SleepReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface SleepFeedBackRepository
-        extends JpaRepository<SleepFeedBack, Long> {
-
+public interface SleepFeedBackRepository extends JpaRepository<SleepFeedBack, Long> {
     boolean existsBySleepReview(SleepReview sleepReview);
     void deleteAllBySleepReviewIn(List<SleepReview> reviews);
+    Optional<SleepFeedBack> findBySleepReviewId(long sleepReviewId);
 }
